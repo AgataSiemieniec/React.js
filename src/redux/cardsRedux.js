adbc970436aca4ad707aadc0c24b318fe4a56a6d
Agata Siemieniec <agata.siemieniec@hotmail.com>
2021-01-13 23:24:48 +0100

    display component SEARCH

--- src/redux/cardsRedux.js
+++ src/redux/cardsRedux.js
@@ -2,6 +2,7 @@ import shortid from 'shortid';
 
 // selectors
 export const getCardsForColumn = ({cards}, columnId) => cards.filter(card => card.columnId == columnId);
+({cards}, searchString) => cards.filter(card => new RegExp(searchString, 'i').test(card.title)).length;
 
 // action name creator
 const reducerName = 'cards';
